bfcea50db4de98796a284263d0bdee4ca2e4e825
AGORA-AD\kosan <kosan@agora.pl>
2018-05-25 14:23:11 +0200

    allow dla options

--- src/main/java/pl/kosan/tin/config/SecurityConfig.java
+++ src/main/java/pl/kosan/tin/config/SecurityConfig.java
@@ -2,20 +2,23 @@ package pl.kosan.tin.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
                 .antMatchers("/*").authenticated()
                 .and()
                 .httpBasic();
